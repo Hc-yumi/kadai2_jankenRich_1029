d230c6172cd3e98f244378fb07869e2a40d609fe
Hc-yumi <hach8686@gmail.com>
2022-10-30 17:24:23 +0900

    10301724

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-const water =$("#water");
-const clothes =$("clothes");
-const furniture =$("furniture");
-const shoes =$("shoes");
-const cosme =$("#cosme");
-
+let x = 0;
+let water = 8;
+let clothes = 6;
+let furniture = 10;
+let shoes = 2;
+let cosme = 4;
 
 $("#water").on("click",function(){
   $("#water").animate({
     "marginLeft":"-300px"
   });
+  console.log(x =+ water);
   $("#water").width("-20%");
   $("#water").fadeOut(2000);
 });
@@ -17,6 +18,7 @@ $("#clothes").on("click",function(){
   $("#clothes").animate({
     "marginLeft":"-300px"
   });
+  console.log(x =+ clothes);
   $("#clothes").width("-20%");
   $("#clothes").fadeOut(2000);
 });
@@ -25,6 +27,7 @@ $("#furniture").on("click",function(){
   $("#furniture").animate({
     "marginLeft":"-300px"
   });
+  console.log(x =+ furniture);
   $("#furniture").width("-20%");
   $("#furniture").fadeOut(2000);
 });
@@ -33,6 +36,7 @@ $("#shoes").on("click",function(){
   $("#shoes").animate({
     "marginLeft":"-300px"
   });
+  console.log(x =+ shoes);
   $("#shoes").width("-20%");
   $("#shoes").fadeOut(2000);
 });
@@ -41,8 +45,11 @@ $("#cosme").on("click",function(){
   $("#cosme").animate({
     "marginLeft":"-300px"
   });
+  console.log(x =+ cosme);
   $("#cosme").width("-20%");
   $("#cosme").fadeOut(2000);
 });
+
+
 
 
