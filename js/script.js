772d2453c9d9374266fc5fcd9f21f6abadaf6253
Hc-yumi <hach8686@gmail.com>
2022-11-03 17:52:57 +0900

    1103_1752

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // 1.対象のモノの個数を選択したら、その定義に入っている数量を加算する
       //(見え方は重量にする)
-// 2.3つ選ぶ
+// 2.選んだものの合計値を出す
 // 3.3つ選んだら非活性にする
 // 4.運ぶものを決めたら決定ボタンを押す
 // 5.決定ボタンを押したら画面が遷移する
@@ -8,7 +8,7 @@
 // 7.トラックの車格を選ぶ
 // 8.車格に応じて所持金が減る
 // 9.2.で決めた荷物の重量よりも小さければOK,重ければNGを出す（アラート）
-// 10.重量よりも重ければペナルティ-¥30,000 ／ 重量よりも軽ければ+¥30,000
+// 10.重量よりも重ければペナルティ-¥100,000 ／ 重量よりも軽ければ+¥100,000
 
 
 // $("#water").on("click",function(){
@@ -44,10 +44,11 @@ $(function() {
     }    
     $("#item_weight_total").val(total + "kg");
 
+
     if( total > 0 && total <= 1000) {
         // alert("軽");
         console.log("軽");
-        let kei = 1000;
+        // let kei = 1000;
         // $("#kei").on("click",function(){
         //   if( total < 1000 ){
         //     alert("OK");
@@ -59,7 +60,7 @@ $(function() {
       } else if (total > 1000 && total <= 2000){
         // alert("2t");
         console.log("2t");
-        let niton = 2000;
+        // let niton = 2000;
         // $("#niton").on("click",function(){
         //   alert("OK");
         // })
@@ -67,7 +68,7 @@ $(function() {
       } else if(total > 2000 && total <= 4000){
         // alert("4t");
         console.log("4t");
-        let yonton = 4000;
+        // let yonton = 4000;
         // $("#yonton").on("click",function(){
         //   alert("OK");
         // })
@@ -75,7 +76,7 @@ $(function() {
       } else if (total > 4000 && total <= 10000){
         // alert("10t")
         console.log("10t");
-        let zyuton = 10000;
+        // let zyuton = 10000;
         // $("#jyuton").on("click",function(){
         //   alert("OK");
         // })
@@ -83,12 +84,12 @@ $(function() {
 
 
       //ローカルストレージを使う方法
-      function saveData(){
-        var score = new scoretotal();
-        localStorage.setItem(total);
-      }
-      localStorage.setItem('key', 'value1');
-      localStorage.saveKey = 'value2';
+      // function saveData(){
+      //   var score = new scoretotal();
+      //   localStorage.setItem(total);
+      // }
+      // localStorage.setItem('key', 'value1');
+      // localStorage.saveKey = 'value2';
 
       //車格判定をする 
       let kei=10000;
@@ -96,81 +97,96 @@ $(function() {
       let yonton=30000;
       let jyutton =50000; 
 
-   console.log(total);
-   
+    
+   console.log(total);  
+  let value = null;
 
+  //軽¥10,000を所持金から引いた金額を表示する
    $("#kei").on("click",function(){
-    alert("OK");
+    // alert("OK");
+    $("#now_money").html('¥90,000');
+
      if( total > 0 && total <= 1000 ){
        alert("OK");
+      //  value = OK;
+       $("#result").hide(2000).fadeIn(5000).html('正しい選定をありがとう！ボーナス¥100,000');
+       $("#result_money").hide().fadeIn(20000).html('+ ¥100,000');
      } else {
        alert("NG");
-     }  
-     
-    //  $("#now_money").html('100000-kei');
+      //  value = NG;
+       $("#result").hide(2000).fadeIn(5000).html('積載オーバーだよ・・。ペナルティマイナス¥100,000・・まだまだだね。');
+       $("#result_money").hide().fadeIn(20000).html('- ¥100,000');
+       
+     }      
+    
    })
  
+  //2t ¥20,000を所持金から引いた金額を表示する
    $("#niton").on("click",function(){
+    $("#now_money").html('¥80,000');
      if( total <= 2000 ){
        alert("OK");
+      //  value = OK;
+       $("#result").hide(2000).fadeIn(5000).html('正しい選定をありがとう！ボーナス¥100,000');
+       $("#result_money").hide().fadeIn(20000).html('+ ¥100,000');
+
      } else {
        alert("NG");
+      //  value = NG;
+       $("#result").hide(2000).fadeIn(5000).html('積載オーバーだよ・・。ペナルティマイナス¥100,000・・まだまだだね。');
+       $("#result_money").hide().fadeIn(20000).html('- ¥100,000');
      }          
    })
- 
+
+  //4t ¥30,000を所持金から引いた金額を表示する 
    $("#yonton").on("click",function(){
+    $("#now_money").html('¥70,000');
      if( total <= 4000 ){
        alert("OK");
+      //  value = OK;
+       $("#result").hide(2000).fadeIn(5000).html('正しい選定をありがとう！ボーナス¥100,000');
+       $("#result_money").hide().fadeIn(20000).html('+ ¥100,000');
      } else {
        alert("NG");
+      //  value = NG;
+       $("#result").hide(2000).fadeIn(5000).html('積載オーバーだよ・・。ペナルティマイナス¥100,000・・まだまだだね。');
+       $("#result_money").hide().fadeIn(20000).html('- ¥100,000');
      }          
    })
- 
+
+
+  //10t ¥50,000を所持金から引いた金額を表示 
    $("#jyuton").on("click",function(){
+    $("#now_money").html('¥50,000');
      if( total <= 10000 ){
        alert("OK");
+      //  value = OK;
+       $("#result").hide(2000).fadeIn(5000).html('正しい選定をありがとう！ボーナス¥100,000');
+       $("#result_money").hide().fadeIn(20000).html('+ ¥100,000');
      } else {
        alert("NG");
+      //  value = NG;
+       $("#result").hide(2000).fadeIn(5000).html('積載オーバーだよ・・。ペナルティマイナス¥100,000・・まだまだだね。');
+       $("#result_money").hide().fadeIn(20000).html('- ¥100,000');
      }          
-   })
-
-   
-
-     
-      
-      
+   })      
 
   });  
+
+  //決定ボタンを押した後の効果音
+  $("#decision").on("click",function(){
+    $("#play_kettei").get(0).play();
+  })
+
 
   //トラックの後ろ姿が動く＋音がなる
   $('#shipping').on("click",function(){
     // alert("OK");
     $("#play_ship").get(0).play(); 
-    $("#back_truck").addClass('yure');
-      
+    $("#back_truck").addClass('yure');    
 
    })
 
-   
-
-   
-
-  
-
-  // click イベントとif文を使って、輸送可否判断のアニメーションつくる！
-  
-  
-  // $("#kei").on("click",function(){
-  //   let keisya;
-  //   if(total < 1000){
-  //     alert("OK");
-  //     $("#result").html("うまく運べたね♪")
-
-  //   }else{
-  //     alert("NG");
-  //     $("#result").html("積載オーバー・・")
-  //   }
-  // }) 
 });
 
 // $(function(){
